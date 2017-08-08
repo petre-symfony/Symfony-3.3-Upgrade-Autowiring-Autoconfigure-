f5032b5f612e0a81d69a68f8fc04804f328a8c1c
petrero <petrero@gmail.com>
2017-08-08 23:21:39 +0300

    Copy code from previous branch

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,7 @@ class AppKernel extends Kernel {
       new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
       new Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle(),
       new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
-      new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+      new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),  
 
       new AppBundle\AppBundle(),
     );
@@ -28,6 +28,7 @@ class AppKernel extends Kernel {
       $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
       $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
       $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+      $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
     }
 
     return $bundles;
