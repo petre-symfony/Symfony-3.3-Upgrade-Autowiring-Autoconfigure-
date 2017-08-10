c90c3ff51d0925276ed3c62f0e75164078c314f1
petrero <petrero@gmail.com>
2017-08-10 21:11:19 +0300

    Our app is now fully using the new config features. It's time to start enjoying it a little!
    Let's start by cleaning up legacy_aliases.yml
    
    Remember, we created this because these old service ids might still be used in our app. Let's eliminate these one-by-one.
    
    Copy the first id and go see where it's used:
    git grep app.markdown_transformer
    
    Private Services and {dollar_sign}container->get()
    
    Ok! This is used in GenusController. Easy fix! Let's change this to use the new service id: MarkdownTransformer::class
    
    Awesome! Let's try it: navigate to /genus. That code was on the show page, so click any of the genuses and... explosion!
    
    You have requested a non-existent service AppBundle\Service\MarkdownTransformer.
    But... we know that's a service: it's even explicitly configured:
    
     46 lines  app/config/services.yml
    ... lines 1 - 8
    services:
    ... lines 10 - 29
      AppBundle\Service\MarkdownTransformer:
        arguments: ['@markdown.parser', '@doctrine_cache.providers.my_markdown_cache']
    ... lines 32 - 46
    What's going on!
    
    Remember, all of these services are private... which means that we cannot fetch them via {dollar_sign}container->get():
    
     46 lines  app/config/services.yml
    ... lines 1 - 8
    services:
      _defaults:
    ... lines 11 - 12
        public: false
    ... lines 14 - 46
    This is actually one of the big motivations behind all of these autowiring and auto-registration changes: we do not want you to fetch things out of the container directly anymore. Nope, we want you to use dependency injection.
    
    Not only is dependency injection a better practice than calling {dollar_sign}container->get(), using it will actually give you better errors!
    
    For example, if you use {dollar_sign}container->get() and accidentally fetch a service that doesn't exist, you'll only get an error if you visit a page that runs that code. But if you use dependency injection and reference a service that doesn't exist, you'll get a huge error when you access any page or try to do anything in your app. If you make all services private, the new config system is actually more stable than the old one.

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -75,7 +75,7 @@ class GenusController extends Controller {
   public function showAction(Genus $genus) {
     $em = $this->getDoctrine()->getManager();
 
-    $markdownTransformer = $this->get('app.markdown_transformer');
+    $markdownTransformer = $this->get(MarkdownTransformer::class);
     $funFact = $markdownTransformer->parse($genus->getFunFact());
 
     $this->get('logger')
